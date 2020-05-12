c70e399b12e26e1940b4f5979576659ef4575ac1
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-05-12 15:47:35 +0100

    save user info into mongo db

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require ('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const User = mongoose.model("User")
 
 router.get('/',(req,res)=> {
     res.send("Hello")
@@ -10,7 +12,29 @@ router.post('/signup',(req,res)=> {
     if(!email || !password || !name) {
         return res.status(422).json({error:"Please complete all fields"})
     }
-    res.json({message:"successfully posted"})
+    User.findOne({email:email})
+    .then((savedUser)=> {
+        if(savedUser){
+            return res.status(422).json({error:"User already exists with this email"})
+        }
+        const user = new User({
+            email,
+            password,
+            name
+        })
+
+        user.save()
+        .then(user=>{
+            res.json({message:"saved successfully"})
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    })
+    .catch(err=>{
+        console.log(err)
+    })
 })
+
 
 module.exports = router
