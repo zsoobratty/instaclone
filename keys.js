da5d5583219161c258bc4505b7c6b3cc455e9b04
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-05-12 16:10:33 +0100

    send token using JWT

--- keys.js
+++ keys.js
@@ -1,3 +1,4 @@
 module.exports={
-    MONGOURI:"mongodb+srv://ziad:BESuycSaRoQIRH0p@cluster0-doxqx.mongodb.net/test?retryWrites=true&w=majority"
+    MONGOURI:"mongodb+srv://ziad:BESuycSaRoQIRH0p@cluster0-doxqx.mongodb.net/test?retryWrites=true&w=majority",
+    JWT_SECRET:"SECRETSQUIRREL"
 }
