b5fd01d1413adef8279be86f62fcbd8e13f9eae6
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-05-26 13:36:32 +0100

    add user route api to view other profiles

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ require('./models/post')
 app.use(express.json())
 app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
+app.use(require('./routes/user'))
 
 
 app.listen(PORT,()=>{
