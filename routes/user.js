cc5a0d72cf70e99474eb27a3aa3ca9bb47490a58
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-05-26 14:25:51 +0100

    add backend for follow and unfollow

--- routes/user.js
+++ routes/user.js
@@ -5,9 +5,9 @@ const requireLogin = require('../middleware/requireLogin')
 const Post = mongoose.model("Post")
 const User = mongoose.model("User")
 
-router.get('/user/:id', (req,res)=>{
+router.get('/user/:id', requireLogin, (req,res)=>{
     User.findOne({_id:req.params.id})
-    select("-password")
+    .select("-password")
     .then(user=>{
         Post.find({postedBy:req.params.id})
         .populate("postedBy", "_id name")
@@ -22,7 +22,41 @@ router.get('/user/:id', (req,res)=>{
     })
 })
 
+router.put('follow',requireLogin,(req,res)=>{
+    User.findByIdAndUpdate(req.body.followId,{
+        $push:{followers:req.user._id}
+    },{new:true},
+    (err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }
+        User.findByIdAndUpdate(req.user._id,{
+            $push:{following:req.body.followId},
+        },{new:true}).then(result=>{
+            res.json(result)
+        }).catch(err=>{
+            return res.status(422).json({error:err})
+        })
+    })
+})
 
+router.put('unfollow',requireLogin,(req,res)=>{
+    User.findByIdAndUpdate(req.body.unfollowId,{
+        $pull:{followers:req.user._id}
+    },{new:true},
+    (err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }
+        User.findByIdAndUpdate(req.user._id,{
+            $pull:{following:req.body.unfollowId},
+        },{new:true}).then(result=>{
+            res.json(result)
+        }).catch(err=>{
+            return res.status(422).json({error:err})
+        })
+    })
+})
 
 
 module.exports = router
