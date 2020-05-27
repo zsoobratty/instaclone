6d0431c7dbbd60f5cb2c5f9016f98d377581c08f
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-05-27 17:12:28 +0100

    add update picture route

--- routes/user.js
+++ routes/user.js
@@ -58,5 +58,15 @@ router.put('/unfollow',requireLogin,(req,res)=>{
     })
 })
 
+router.put('/updatepic',requireLogin,(req,res)=>{
+    User.findByIdAndUpdate(req.user._id, {$set:{pic:req.body.pic}},{new:true},
+        (err,result)=>{
+            if(err){
+            return res.status(422).json({error:"Picture cannot be updated"})
+        }
+        res.json(result)
+    })
+})
+
 
 module.exports = router
