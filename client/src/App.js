fc65777ad98d9ff6c8546c2fe9d41fef36f2d016
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-06-02 14:13:51 +0100

    add client folder to git repo

---
+++ client/src/App.js
@@ -0,0 +1,68 @@
+import React,{useEffect, createContext,useReducer, useContext} from 'react';
+import NavBar from './components/NavBar'
+import './App.css'
+import {BrowserRouter,Route,Switch,useHistory} from 'react-router-dom'
+import Home from './components/screens/Home'
+import Profile from './components/screens/Profile'
+import SignUp from './components/screens/Signup'
+import SignIn from './components/screens/Signin'
+import CreatePost from './components/screens/CreatePost'
+import {reducer,initialState} from './reducers/userReducer'
+import UserProfile from './components/screens/UserProfile'
+import FollowedPosts from './components/screens/FollowedPosts'
+
+export const UserContext = createContext()
+
+const Routing = ()=>{
+  const history = useHistory()
+  const {state, dispatch} = useContext(UserContext)
+  useEffect(()=>{
+    const user = JSON.parse(localStorage.getItem("user"))
+
+    if(user){
+      dispatch({type:"USER",payload:user})
+    } else {
+      history.push('/signin')
+    }
+  },[])
+  return(
+    <Switch>
+    <Route exact path="/" >
+    <Home />
+    </Route>
+    <Route path="/signin">
+      <SignIn />
+    </Route>
+    <Route path="/signup">
+      <SignUp />
+    </Route>
+    <Route exact path="/profile">
+      <Profile />
+    </Route>
+    <Route path="/create">
+      <CreatePost />
+    </Route>
+    <Route path="/profile/:userid">
+      <UserProfile />
+    </Route>
+    <Route path="/myfollowingpost">
+      <FollowedPosts />
+    </Route>
+    </Switch>
+  )
+}
+
+
+function App() {
+  const [state,dispatch] = useReducer(reducer,initialState)
+  return (
+    <UserContext.Provider value={{state,dispatch}}>
+    <BrowserRouter>
+      <NavBar />
+      <Routing />
+    </BrowserRouter>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
---
+++ client/src/components/screens/Profile.js
@@ -0,0 +1,106 @@
+import React,{useEffect,useState,useContext} from "react";
+import {UserContext} from '../../App'
+
+const Profile = () => {
+  const [myPics,setPics] = useState([])
+  const {state,dispatch} = useContext(UserContext)
+  const [image,setImage] = useState("")
+  useEffect(()=>{
+    fetch('/myposts',{
+      headers:{
+        "Authorisation":"Bearer "+localStorage.getItem('jwt')
+      }
+    }).then(res=>res.json())
+    .then(result=>{
+      setPics(result.myPost)
+    })
+  })
+
+  useEffect(()=>{
+    if(image){
+      const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "instaclone");
+    data.append("cloud_name", "cloudziad");
+    fetch("	https://api.cloudinary.com/v1_1/cloudziad/image/upload", {
+      method: "post",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        fetch('/updatepic',{
+          method:'put',
+          headers:{
+            "Content-Type":"application/json",
+            "Authorisation":"Bearer "+localStorage.getItem('jwt')
+          },
+          body:JSON.stringify({
+            pic:data.url
+          })
+        }).then(res=>res.json())
+        .then(result=>{
+          console.log(result)
+          localStorage.setItem("user",JSON.stringify({...state,pic:result.pic}))
+          dispatch({type:"UPDATEPIC",payload:result.pic})
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+  },[image])
+
+  const updatePhoto = (file)=>{
+    setImage(file)
+  }
+  return (
+    <div style={{maxWidth:"550px",margin:"0px auto"}}>
+      <div style={{
+            margin:"18px 0px",
+            borderBottom:"1px solid grey"
+          }}>
+        <div style={{
+            display:"flex",
+            justifyContent:"space-around",
+          }}>
+        <div>
+          <img
+            style={{ width: "160px", height: "160px", borderRadius: "80px" }}
+            src={state?state.pic:"loading"}
+          />
+        </div>
+        <div>
+          <h4>{state?state.name:"loading"}</h4>
+          <h5>{state?state.email:"loading"}</h5>
+          <div style={{display:"flex", justifyContent:"space-between", width:"108%"}}>
+            <h6>{myPics.length} Posts</h6>
+            <h6>{state?state.followers.length:"0"} Followers</h6>
+            <h6>{state?state.following.length:"0"} Following</h6>
+          </div>
+        </div>
+      </div>
+        <div className="file-field input-field" style={{margin:"10px"}}>
+        <div className="btn #64b5f6 blue darken-1">
+          <span>Upload Profile Photo</span>
+          <input type="file" onChange={(e) => updatePhoto(e.target.files[0])} />
+        </div>
+        <div className="file-path-wrapper">
+          <input className="file-path validate" type="text" />
+        </div>
+      </div>
+      </div>
+        <div className="gallery">
+          {
+            myPics.map(item=>{
+              return(
+            <img key={item._id} className="item" src={item.photo} alt={item.title}/>
+
+              )
+            })
+          }
+        </div>
+    </div>
+  );
+};
+
+export default Profile;
